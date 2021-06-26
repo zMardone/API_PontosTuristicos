99e59ff9c3e679ac04a2d0b24b33feba08b64ac1
zMardone <mardone33@gmail.com>
2021-06-26 18:36:07 -0300

    ajustes config

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from typing import cast
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,5 @@ MEDIA_ROOT = 'imagens'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
